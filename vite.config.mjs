4785150dd0944d1ca7a4568e48ba4cf6a01346c0
Ethan K <ethvinkis.06@gmail.com>
2026-03-04 07:37:12 -0700

    moved index to public for better organization

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'public',               // source index.html lives in public/
   plugins: [react()],
-  base: '/norse-stargate-asgard/'
+  base: '/norse-stargate-asgard/',
+  build: {
+    outDir: '../dist',          // write dist to project root /dist
+    emptyOutDir: true
+  }
 })
