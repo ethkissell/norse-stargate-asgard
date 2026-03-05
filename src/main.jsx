a18f4c6a1e756fe6d0fa44c6ffda27ece3ee8e88
Ethan K <ethvinkis.06@gmail.com>
2026-03-05 12:51:58 -0700

    moved components css to src

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import "./app.css";
-import "./components/icon-nav.css"
+import "./icon-nav.css"
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
