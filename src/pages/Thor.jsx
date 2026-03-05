b2d9272d03f2cedbc22f0bec9c11b34ac978e08f
Ethan K <ethvinkis.06@gmail.com>
2026-03-05 12:55:01 -0700

    updated Thor page

--- src/pages/Thor.jsx
+++ src/pages/Thor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Thor() {
     return (
         <div>
-            <h3 className='contentTitle'>God of the Working Class Compared to the Supreme Comander of the Asgard</h3>
+            <h3 className='contentTitle'>Thor</h3>
             <p>Thor content here.</p>
         </div>
     );
