ca2539f66c2a1cecadbfe92e4e42bb412a6c4bf5
Ethan K <ethvinkis.06@gmail.com>
2026-03-26 19:58:01 -0600

    added citations page

---
+++ src/pages/Citations.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Citations() {
+  return (
+    <div className="citations-page">
+      <h3 className="contentTitle">Citations</h3>
+
+      <p className="citation">
+        <i>Gylfaginning</i>, Snorri Sturluson, and Anthony Faulkes. <i>Edda</i>. Everyman, 1996.
+      </p>
+
+      <p className="citation">
+        <i>Harbard's Song</i>, <i>The Poetic Edda</i>. Translated by Carolyne Larrington, Oxford, Oxford University Press, 2014.
+      </p>
+
+      <p className="citation">
+        Intro in Mathias Nordvig. <i>Old Norse Mythology</i>. Taylor &amp; Francis, 22 Dec. 2025.
+      </p>
+
+      <p className="citation">
+        <i>Voluspa</i>, <i>The Poetic Edda</i>. Translated by Carolyne Larrington, Oxford, Oxford University Press, 2014.
+      </p>
+
+      <p className="citation">
+        <i>Stargate SG-1</i>. Created by Brad Wright and Jonathan Glassner, performances by Richard Dean Anderson, Amanda Tapping, Michael Shanks, Christopher Judge, and others, MGM Television, 1997–2007.
+      </p>
+
+      <p className="citation">
+        <i>Stargate Atlantis</i>. Created by Brad Wright and Robert C. Cooper, performances by Joe Flanigan, Rachel Luttrell, Jason Momoa, David Hewlett, and others, MGM Television, 2004–2009.
+      </p>
+    </div>
+  );
+}
