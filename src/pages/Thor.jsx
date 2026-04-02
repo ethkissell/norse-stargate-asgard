00212b5b5fa5d581ea4fe8d100dba135ea024291
Ethan K <ethvinkis.06@gmail.com>
2026-04-02 08:22:49 -0600

    prepped Thor page

--- src/pages/Thor.jsx
+++ src/pages/Thor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Thor() {
     return (
         <div>
-            <h3 className='contentTitle'>Thor</h3>
+            <h3 className='contentTitle'>Thor: The God of the People</h3>
             <p>Thor content here.</p>
         </div>
     );
