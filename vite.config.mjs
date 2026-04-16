0604ba4aebe96b8e571ac5ff0d0710f1adf313cb
evkissell <111837728+ethkissell@users.noreply.github.com>
2026-04-16 17:21:45 -0600

    Change base path for Vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/norse-stargate-asgard/',
+  base: "/old-norse-capstone/",
   server: {
     port: 8000,
     strictPort: true
